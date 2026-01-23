c561bdd5d75594114a7e53d441685d3c5525dd98
hazyfossa <hazyfossa@gmail.com>
2026-01-23 22:59:52 +0500

    Add kanal interop for in-memory channels

--- src/compat.rs
+++ src/compat.rs
@@ -22,3 +22,36 @@ mod futures {
         }
     }
 }
+
+#[cfg(feature = "kanal")]
+pub mod kanal {
+    use crate::ChannelError;
+
+    use kanal::{AsyncReceiver, AsyncSender};
+
+    pub struct Channel<T> {
+        tx: AsyncSender<T>,
+        rx: AsyncReceiver<T>,
+    }
+
+    impl<T> crate::Channel for Channel<T> {
+        type Wire = T;
+        async fn recv(&mut self) -> Result<Self::Wire, ChannelError> {
+            self.rx.recv().await.map_err(|_| ChannelError::Closed)
+        }
+
+        async fn send(&mut self, data: Self::Wire) -> Result<(), ChannelError> {
+            self.tx.send(data).await.map_err(|_| ChannelError::Closed)
+        }
+    }
+
+    pub fn channel_pair<T>(size: usize) -> (Channel<T>, Channel<T>) {
+        let (a_tx, a_rx) = kanal::bounded_async(size);
+        let (b_tx, b_rx) = kanal::bounded_async(size);
+
+        let a = Channel { tx: a_tx, rx: b_rx };
+        let b = Channel { tx: b_tx, rx: a_rx };
+
+        (a, b)
+    }
+}
